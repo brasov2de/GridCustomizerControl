d5eb680e3997ccf62f691e9992f961c22d0dc610
Diana <diana.birkelbach@orbis.de>
2022-10-26 11:57:28 +0200

    config changes

---
+++ RelatedRecords/webpack.config.js
@@ -0,0 +1,7 @@
+
+// eslint-disable-next-line no-undef
+module.exports = {    
+    watch: true, // uncomment this line to enable webpack watch mode
+    //devtool: "inline-source-map"   
+    devtool: "inline-source-map"   
+};
